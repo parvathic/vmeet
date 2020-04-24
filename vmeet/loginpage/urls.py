5077b015dadd9deb31336886b6c068ffb185f65b
parvathicgit config --global user.namegit config --global user.name parvathic <parvathic@vmware.com>
2020-04-23 22:18:08 -0700

    Revert "added loginpage app"
    This reverts commit 9aa20be9ff7d7c6da4f0c44ac0992b986b3ca728.

--- vmeet/loginpage/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from loginpage import views
-from django.contrib.auth import views as auth_views
-
-urlpatterns = [
-    url(r'^$', views.loginpage),
-    url(r'^login/$', auth_views.LoginView.as_view(template_name="loginpage/login.html"), name="login")
-]
--- vmeet/loginpage/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-def loginpage(request):
-    username = ['parvathic', 'user2']
-    password = ['12345', 'hello']
-    args = {'key': password}
-    return render(request, 'loginpage/home.html', args)
