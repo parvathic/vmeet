fef49d38ce24288ab80ae4185ba606d00c47de9b
Urmil Parikh <uparikh@ncsu.edu>
2020-04-24 11:24:27 -0700

    changes #2

--- vmeet/vmeet/urls.py
+++ vmeet/vmeet/urls.py
@@ -20,21 +20,14 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
-    path('login/index.html/game.html/', include('game.urls')),
-    path('login/index.html/', include('homepage.urls')),
+    path('login/index/game/', include('game.urls')),
+    path('login/index/', include('homepage.urls')),
     path('login/', include('loginpage.urls')),
     path('login/signup.html', include('loginpage.urls')),
+    path('', RedirectView.as_view(url='login/', permanent=True)),
 ]
 
-=======
-    path('index/game.html/', include('game.urls')),
-    path('index/', include('homepage.urls')),
-]
-urlpatterns += [
-    path('', RedirectView.as_view(url='index/', permanent=True)),
-]
->>>>>>> 203420c2a534455315f4b77a9f1d069abc7df643
+
 # Use static() to add url mapping to serve static files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
